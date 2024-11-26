8bd0d19f6d4248f597c111733b874543a8a78cf4
Javier Cruz Bonoso <javier.cruz.bonoso@avanade.com>
2024-11-26 08:59:25 +0100

    refactor: move counter logic to MyCounter component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,9 @@
 <template>
-  <section>
-    <h3>Counter: {{ counter }}</h3>
-    <h3>Square: {{ squareCounter }}</h3>
-
-    <div>
-      <button @click="increment">+1</button>
-      <button @click="decrement">-1</button>
-    </div>
-  </section>
+  <h1>Mi primera app</h1>
+  <hr />
+  <MyCounter />
 </template>
 
 <script lang="ts" setup>
-import { computed, ref } from 'vue'
-
-const counter = ref(2)
-const squareCounter = computed(() => counter.value * counter.value)
-
-const increment = () => {
-  counter.value++
-}
-
-const decrement = () => {
-  counter.value--
-}
+import MyCounter from './components/MyCounter.vue'
 </script>
---
+++ src/components/MyCounter.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <h3>Counter: {{ counter }}</h3>
+    <h3>Square: {{ squareCounter }}</h3>
+
+    <div>
+      <button @click="increment">+1</button>
+      <button @click="decrement">-1</button>
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import { computed, ref } from 'vue'
+
+const counter = ref(2)
+const squareCounter = computed(() => counter.value * counter.value)
+
+const increment = () => {
+  counter.value++
+}
+
+const decrement = () => {
+  counter.value--
+}
+</script>
