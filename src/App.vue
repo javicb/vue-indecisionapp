b11899eb2571b68365ba93f770f4624ffb002293
Javier Cruz Bonoso <javier.cruz.bonoso@avanade.com>
2024-11-27 09:49:09 +0100

    feat: implementar el diseño del chat con los componentes ChatMessages y MessageBox

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <template>
-  <h1 className="text-3xl font-bold underline">Hello world!</h1>
-  <hr />
-  <MyCounter :initialCounter="1" />
-  <MyCounterScript :initialCounter="2" />
-  <MyCounterScript2 :initialCounter="3" />
+  <IndecisionView />
 </template>
 
 <script lang="ts" setup>
-import MyCounter from './components/MyCounter.vue'
-import MyCounterScript from './components/MyCounterScript.vue'
-import MyCounterScript2 from './components/my-counter-script/MyCounterScript2.vue'
+import IndecisionView from '@/views/IndecisionView.vue'
 </script>
---
+++ src/views/IndecisionView.vue
@@ -0,0 +1,17 @@
+<!-- Fuente: https://tailwindcomponents.com/component/chat-layout -->
+<template>
+  <div class="bg-gray-100 h-screen flex flex-col max-w-lg mx-auto">
+    <div class="bg-blue-500 p-4 text-white flex justify-between items-center">
+      <span>Chat</span>
+    </div>
+
+    <ChatMessages />
+
+    <MessageBox />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import ChatMessages from '@/components/chat/ChatMessages.vue'
+import MessageBox from '@/components/chat/MessageBox.vue'
+</script>
