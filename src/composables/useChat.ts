4314c1e3db2188f1b3175c6e3be64188372a8a5a
Javier Cruz Bonoso <javier.cruz.bonoso@avanade.com>
2024-11-27 11:18:39 +0100

    feat: añadir lógica para obtener respuestas de sí/no y actualizar mensajes en el chat

--- src/composables/useChat.ts
+++ src/composables/useChat.ts
@@ -1,14 +1,33 @@
 import type { ChatMessage } from '@/interfaces/chat-message-interface'
+import type { YesNoResponse } from '@/interfaces/yes-no-response'
 import { ref } from 'vue'
 
 export const useChat = () => {
   const messages = ref<ChatMessage[]>([])
+  const getResponse = async () => {
+    const response = await fetch('https://yesno.wtf/api')
+    const data = (await response.json()) as YesNoResponse
+    return data
+  }
 
-  const onMessage = (message: string) => {
+  const onMessage = async (message: string) => {
+    if (message.length === 0) return
+
     messages.value.push({
       id: messages.value.length + 1,
       message,
       itsMine: true,
+    })
+
+    if (!message.endsWith('?')) return
+
+    const { answer, image } = await getResponse()
+
+    messages.value.push({
+      id: messages.value.length + 1,
+      message: answer,
+      image,
+      itsMine: false,
     })
   }
 
