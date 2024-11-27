09f488257a683399345957e835feeacc98262179
Javier Cruz Bonoso <javier.cruz.bonoso@avanade.com>
2024-11-27 11:41:40 +0100

    feat: añadir desplazamiento automático en ChatMessages al recibir nuevos mensajes

--- src/components/chat/ChatMessages.vue
+++ src/components/chat/ChatMessages.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex-1 overflow-y-auto p-4">
+  <div ref="chatRef" class="flex-1 overflow-y-auto p-4">
     <div class="flex flex-col space-y-2">
       <ChatBubble
         v-for="message in messages"
@@ -12,11 +12,23 @@
 
 <script lang="ts" setup>
 import type { ChatMessage } from '@/interfaces/chat-message-interface'
+import { ref, watch } from 'vue'
 import ChatBubble from './ChatBubble.vue'
 
 interface Props {
   messages: ChatMessage[]
 }
 
-defineProps<Props>()
+const props = defineProps<Props>()
+
+const chatRef = ref<HTMLDivElement | null>(null)
+
+watch(props.messages, () => {
+  setTimeout(() => {
+    chatRef.value?.scrollTo({
+      top: chatRef.value?.scrollHeight,
+      behavior: 'smooth',
+    })
+  }, 100)
+})
 </script>
