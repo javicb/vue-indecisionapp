0d7d2344807f44991a5f7e1f2408d540da2fa0b1
Javier Cruz Bonoso <javier.cruz.bonoso@avanade.com>
2024-11-28 13:21:31 +0100

    test: añadir imports a los tests

--- tests/components/MyCounter.test.ts
+++ tests/components/MyCounter.test.ts
@@ -1,5 +1,6 @@
 import { mount, VueWrapper } from '@vue/test-utils'
 import MyCounter from '@/components/MyCounter.vue'
+import { afterEach, beforeEach, describe, expect, it } from 'vitest'
 
 describe('MyCounter.vue', () => {
   let wrapper: VueWrapper<unknown>
